3a0fdb997dff3e2495fd71a9838857e4656da744
quasur <adam.corness@gmail.com>
2022-11-15 21:13:57 +0000

    finished simple sim method of pos over time

--- first.py
+++ first.py
@@ -1,21 +1,40 @@
+#%%
 """
 Created by Adam C 12:42 14/11/2022
 
 """
-#%%
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-pos = [[1,0,0],[-1,0,0]]
-vel = [[0,1,0],[0,-1,0]]
-mass= [   1   ,    1   ]
+pos = np.array([[1,0,0],[-1,0,0]])
+vel = np.array([[0,1,0],[0,-1,0]])
+mass= np.array([   1   ,    1   ])
 N = np.size(mass)
-acc = [[1,2,3],[4,5,6 ]]
-dt  = 1
-r2=np.zeros(N)
+r=np.zeros((N,3))
+acc = r
+vel = acc
 
+dt = 1
 tsteps = 100
-print(acc[:,1])
-for a in range(N):
-    r2[a] = np.sum((np.abs(pos - pos[:,a].reshape((3,1))))**2,axis=0)
-    #acc[:,a] = 
+posTime = np.zeros((N,3,tsteps))
+
+for b in range(tsteps):
+    for a in range(N):
+        r = pos[a,:]-pos[:,:]
+        r[a,:] = r[a,:]+10000
+        rnorm=np.linalg.norm(r,axis = 1)
+        rdir = r/rnorm[:,None]
+        r2 = np.sum(r**2,axis=1)
+        acc[a,:]= np.dot((mass/r2),(rdir))
+        
+        vel = vel + acc*dt
+        pos = pos + vel*dt
+        posTime[a,:,b] = pos[a]
+        
+        
+t = np.arange(0,tsteps)
+
+
+
+# %%
